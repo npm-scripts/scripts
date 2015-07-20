babd7489783682273281849eb6f52e8d3d219332
Christoph Hermann <christoph.hermann@kaba.com>
2015-07-20 22:19:56 +0200

    use doxie.appen

--- .doxie.render.toc.js
+++ .doxie.render.toc.js
@@ -1,5 +1,3 @@
-var scripts = require('./scripts.json');
-
 // from https://gist.github.com/mathewbyrne/1280286
 slugify = function(text){
   return text.toString().toLowerCase()
@@ -15,7 +13,6 @@ var render = function(data) {
 
   var out = '* [' + data.title + '](https://github.com/npm-scripts/scripts#' + slugify(data.title) + ')\n';
 
-  if (scripts[scripts.length -1].title === data.title) out = out + '\n';
   return out;
 };
 
